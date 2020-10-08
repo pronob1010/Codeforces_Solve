b2fb1545ca010949fed03347463e3f04ebd7a382
Pronob <pronobmozumder.info@gmail.com>
2020-10-08 23:52:12 +0600

    Done

--- cf_237_a.py
+++ cf_237_a.py
@@ -1,28 +1,7 @@
-t = int(input())
-cashes = 0
-time = []
-for i in range(t):
-    h = list(map(int, input().split()))
-    time.append(h)
+d={}
+for i in range(int(input())):
+	s=input()
+	d[s]=d.get(s,0)+1
 
-
-n = []
-
-for j in range(len(time)):
-    p = 0
-    for k in range(len(time)):
-
-        if j!=k and time[j]==time[k]:
-            cashes+=1
-            p += 1
-            n.append(time[j])
-            print(time[j])
-
-print(p,len(n))
-print(n)
-print(time)
-x = (cashes-(cashes//2))
-if x <=1:
-    print(x+1)
-else:
-    print(x)
+# print(d)
+print(max(d.values()))
