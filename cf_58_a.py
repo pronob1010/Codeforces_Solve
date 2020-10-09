f09cd7988d567599c3de7a3305b9087f561bf4f4
Pronob <pronobmozumder.info@gmail.com>
2020-10-09 17:23:40 +0600

    Done

---
+++ cf_58_a.py
@@ -0,0 +1,20 @@
+s = input().lower()
+# s1= set(s)
+s1 = []
+s2 = ['h','e','l','l','o']
+j=1
+ls2= len(s2)
+for i in range(len(s)):
+    if (j <= ls2):
+        if (s[i] == s2[j - 1]):
+            # print(s[i], i, j)
+            j += 1
+    else:
+        break
+
+
+if j-1 == ls2:
+    print("YES")
+else:
+    print("NO")
+    # xqjqmenkodmlhzyzmmvofdngktygbbxbzpluzcohohmalkoeuwfikblltaaigv
