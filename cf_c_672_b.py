9e69ce9ef8532faab5b3b7e8edc80ffa0a907f49
Pronob <pronobmozumder.info@gmail.com>
2020-10-10 03:19:08 +0600

    with optimal

---
+++ cf_c_672_b.py
@@ -0,0 +1,39 @@
+t = int(input())
+for j in range(t):
+    n = int(input())
+    nlist = list(map(int, input().split()))[:n]
+    d = {}
+    for i in range(n):
+        # print(bin(nlist[i]))
+        k = len(bin(nlist[i]))
+        if k in d:
+            d[k]+=1
+        else:
+            d[k]=1
+
+    ans = 0
+    # print(d)
+    for i in d:
+        # print(d[i],d[i]-1)
+        ans += (d[i]*(d[i]-1))//2
+    print(ans)
+
+
+
+
+
+# t = int(input())
+# for j in range(t):
+#     n = int(input())
+#     nlist = list(map(int, input().split()))[:n]
+#     c = 0
+#     for i in nlist:
+#         for k in nlist:
+#             if not (i == k):
+#                 if i < k and ((i & k) > (i ^ k)):
+#                     c += 1
+#
+#     if len(set(nlist)) == 1 and len(nlist) > 1:
+#         print(n)
+#     else:
+#         print(c)
