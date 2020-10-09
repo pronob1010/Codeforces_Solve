dc62ecf4f7c0d0eede964b5e8a00cc72192e2b64
Pronob <pronobmozumder.info@gmail.com>
2020-10-09 16:01:50 +0600

    Done

--- cf_112_a.py
+++ cf_112_a.py
@@ -1,10 +1,14 @@
-s1 = input()
-s2 = input()
-# for i in range(len(s1)):
-#     max
-if max(s1)>max(s2):
-    print("1")
-elif max(s1)<max(s2):
-    print("-1")
-else:
-    print("0")
+s1 = input().lower()
+s2 = input().lower()
+
+# print(max(s1), max(s2))
+# if max(s1)>max(s2):
+#     print("1")
+# elif max(s1)<max(s2):
+#     print("-1")
+# else:
+#     print("0")
+
+print(s1,s2)
+
+print((s1>s2)-(s1<s2))
