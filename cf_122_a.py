18fcbed12a1707a50fa3fd5504cd182cc38760f8
Pronob <pronobmozumder.info@gmail.com>
2020-10-09 11:15:27 +0600

    Done

--- cf_122_a.py
+++ cf_122_a.py
@@ -1,13 +1,5 @@
-n = input()
-f = 0
-for i in range(len(n)):
-    if n[0]== '4' or n[0]== '7':
-        if n[i]=='4' or n[i] == '7':
-            f=1
-        else:
-            break
-
-if f == 0:
-    print("NO")
-else:
+n = int(input())
+if ((n%4 == 0 ) or (n%7==0) or (n%47==0)):
     print("YES")
+else:
+    print("NO")
--- cf_233_a.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-if ((n%4 == 0 ) or (n%7==0) or (n%47==0)):
-    print("YES")
-else:
-    print("NO")
