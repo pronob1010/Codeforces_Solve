4d1d0b9c2fb59feb36b6bff3cfdb48c4e2802217
Pronob <pronobmozumder.info@gmail.com>
2020-10-09 11:23:17 +0600

    Done

--- cf_122_a.py
+++ cf_122_a.py
@@ -1,5 +1,12 @@
 n = int(input())
-if ((n%4 == 0 ) or (n%7==0) or (n%47==0)):
+f = 0
+ln = [4,7,44,47,74,77,444,447,474,477,744,747,774,777]
+for i in ln:
+    if (n%i == 0 ):
+        f = 1
+        break
+
+if (f == 1):
     print("YES")
 else:
     print("NO")
