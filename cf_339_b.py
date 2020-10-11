7b158fe3691c6686ff223316528f73d70dc79d21
Pronob <pronobmozumder.info@gmail.com>
2020-10-11 12:07:04 +0600

    solved

---
+++ cf_339_b.py
@@ -0,0 +1,27 @@
+a,b = list(map(int, input().split()))
+s = list(map(int, input().split()))[:b]
+r = s[0]
+for i in range(2, len(s)):
+    if s[i-1]==s[i]:
+        r+=0
+    elif s[i-1]>s[i]:
+        r += s[i-1]
+    elif s[i-1]<s[i]:
+        r+=(a-s[i-2])+s[i-1]
+
+
+print(r)
+
+
+# c = []
+# m = max(s)
+# i=0
+# r = 0
+# while len(c)!=len(s):
+#     if s[i] not in c:
+#         c.append(s[i])
+#     else:
+#         c.append(1)
+#     i+=1
+# print(c)
+# print(sum(c))
