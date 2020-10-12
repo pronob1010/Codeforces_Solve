f9d12925eed98d638f664c09c81a20389902dc03
Pronob <pronobmozumder.info@gmail.com>
2020-10-12 22:00:21 +0600

    solved

--- cf_1359_a.py
+++ cf_1359_a.py
@@ -2,9 +2,12 @@ for _ in range(int(input())):
     n, m , k = list(map(int,input().split()))
     e = n//k
     p = 0
-    if e>m:
+    if e>=m:
         p = m
     else:
-        p = m - e
-
+        m-= e
+        if m>1:
+            p = e-(m-1)
+        else:
+            p = e - (m)
     print(p)
