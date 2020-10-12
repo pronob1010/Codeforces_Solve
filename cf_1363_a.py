a1c4b1d81c2f25f5f94f10e05c5fa47e198e5fa5
Pronob <pronobmozumder.info@gmail.com>
2020-10-12 18:05:20 +0600

    solved

--- cf_1363_a.py
+++ cf_1363_a.py
@@ -1,19 +1,36 @@
 for i in range(int(input())):
     n,r = list(map(int, input().split()))
     s = list(map(int, input().split()))[:n]
+    result = 0
+    if len(s)>1:
+        even = []
+        odd = []
+        for j in s:
+            if j%2 == 0:
+                even.append(j)
+            else:
+                odd.append(j)
 
-    c = 0
-    re = s[0]
-    if len(s) != r:
-        for j in range(2,len(s)):
+        result = odd[0]
+        r-=1
+        if r%2 == 0 and len(odd)-1>0:
+            for j in range(2,r):
+                result += odd[j-1]+odd[j]
+                r -= 2
+                j+=1
+        elif r%2==1 and len(odd)-1>0 :
+            for j in range(1,r-1):
+                result += odd[j-1]+odd[j]
+                j+=1
+                r -= 2
+        for k in range(1,r):
+            result+=even[k-1]
+    else:
+        result += s[0]
 
-            if c<=r and ((re+s[j-1])%2==1):
-                re += s[j-1]
-                c += 1
-    elif len(s) == r:
-        re = sum(s)
 
-    if re%2==1:
+
+    if result%2==1:
         print("Yes")
     else:
         print("No")
