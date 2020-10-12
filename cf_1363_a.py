bedcd06bddf895fd5d04bea1baedd796060d876b
Pronob <pronobmozumder.info@gmail.com>
2020-10-12 17:35:53 +0600

    solved

--- cf_1363_a.py
+++ cf_1363_a.py
@@ -3,11 +3,16 @@ for i in range(int(input())):
     s = list(map(int, input().split()))[:n]
 
     c = 0
-    re = 0
-    for j in s:
-        if c<=r:
-            re += j
-            c += 1
+    re = s[0]
+    if len(s) != r:
+        for j in range(2,len(s)):
+
+            if c<=r and ((re+s[j-1])%2==1):
+                re += s[j-1]
+                c += 1
+    elif len(s) == r:
+        re = sum(s)
+
     if re%2==1:
         print("Yes")
     else:
