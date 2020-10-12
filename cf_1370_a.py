4587d9a160bd1b4ba6a80a840f2fdfe21ddcec7a
Pronob <pronobmozumder.info@gmail.com>
2020-10-12 22:54:18 +0600

    solved

--- cf_1370_a.py
+++ cf_1370_a.py
@@ -1,15 +1,16 @@
 for _ in range(int(input())):
     n = int(input())
-    m = 0
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            if i>j:
-                a=i
-                b=j
-                while b:
-                    a,b = b,a%b
-
-                if a>=m:
-                    m = a
+    m = n//2
+    # m = 0
+    # for i in range(1,n+1):
+    #     for j in range(1,n+1):
+    #         if i>j:
+    #             a=i
+    #             b=j
+    #             while b:
+    #                 a,b = b,a%b
+    #
+    #             if a>=m:
+    #                 m = a
     print(m)
     m = 0
