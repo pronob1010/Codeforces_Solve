482e52dc23226272f03144ed098f3187d1d53220
Pronob <pronobmozumder.info@gmail.com>
2020-10-13 14:27:10 +0600

    solved

--- cf_1380_a.py
+++ cf_1380_a.py
@@ -1,17 +1,11 @@
 for _ in range(int(input())):
     n  = int(input())
     s = list(map(int, input().split()))[:n]
-
-    c = 0
-    ns = []
-    for i in range(2,n-1):
-        if s[i-1]<s[i]>s[i+1]:
-            c = 0
+    for i in range(1,n-1):
+        if s[i-1]<s[i] and s[i]>s[i+1]:
             print("YES")
-            print(s[i-1],s[i],s[i+1])
+            print(i,i+1,i+2)
             break
-        else:
-            c = 1
-    if c == 1:
+    else:
         print("NO")
-    print()
+
