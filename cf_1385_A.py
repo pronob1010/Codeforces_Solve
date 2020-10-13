8818e125d4bd2c4fd384d428f4b8e372457258a0
Pronob <pronobmozumder.info@gmail.com>
2020-10-13 19:22:30 +0600

    solved

---
+++ cf_1385_A.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    s = list(map(int, input().split()))
+    s.sort()
+    # a = s[2]
+    # b = s[0]-s[1])
+    # c = abs(s[1]-s[2])
+    if s[1]==s[2]:
+        print("YES")
+        print(s[2],s[0],s[0])
+    else:
+        print("NO")
+
+ 
