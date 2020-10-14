e03a39b9951d64f2962be149b33a0f10638cce59
Pronob <pronobmozumder.info@gmail.com>
2020-10-14 20:24:28 +0600

    solved

---
+++ cf_1393_a.py
@@ -0,0 +1,12 @@
+for i in range(int(input())):
+    n = int(input())
+    if n==1:
+        print(n)
+    elif n==2:
+        print(n)
+
+    elif(n+1)%2==0:
+        print((n+1)//2)
+
+    elif(n+2)%2==0:
+        print((n+2)//2)
