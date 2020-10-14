d94c161ba7013e582985f4b599859b805e9a8219
Pronob <pronobmozumder.info@gmail.com>
2020-10-14 16:43:37 +0600

    solved

--- cf_110_A.py
+++ cf_110_A.py
@@ -1,13 +1,17 @@
 n = input()
 f = 0
 t = int(n)
-while(t!=0):
-    if t%10==4 or t%10 == 7:
-        f =1
-    else:
-        f =0
-        break
-    t//=10
+if len(n)!=1:
+    while(t!=0):
+        if t%10==4 or t%10 == 7:
+            f =1
+        else:
+            f =0
+            break
+        t//=10
+else:
+    if int(n[0])==7:
+        f = 0
 
 if f ==1:
     print("YES")
