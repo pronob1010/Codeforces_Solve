4b56374cdd52aef61404c1f13bb572c8d5cbf4a0
Pronob <pronobmozumder.info@gmail.com>
2020-10-14 21:13:51 +0600

    solved

---
+++ cf_80_A.py
@@ -0,0 +1,33 @@
+def prime(n):
+    if (n <= 1):
+        return False
+    if (n <= 3):
+        return True
+
+    if (n % 2 == 0 or n % 3 == 0):
+        return False
+
+    i = 5
+    while (i * i <= n):
+        if (n % i == 0 or n % (i + 2) == 0):
+            return False
+        i = i + 6
+
+    return True
+
+n,m = list(map(int,input().split()))
+t=n+1
+
+while True:
+
+    if (prime(t)):
+        if t==m:
+            print("YES")
+        else:
+            print("NO")
+        break
+    else:
+        t+=1
+
+
+
