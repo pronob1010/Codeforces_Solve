e89cef81a92af8cf44f47a212e5cae9460ac17c4
Pronob <pronobmozumder.info@gmail.com>
2020-10-14 16:17:23 +0600

    solved

--- cf_59_a.py
+++ cf_59_a.py
@@ -3,14 +3,19 @@ s = [str(st[i]) for i in range(len(st))]
 x= len(s)
 u=0
 l=0
-for i in range(1,x):
-    if s[i-1]>='A' and s[i]<='Z':
+for i in range(1,x+1):
+    if s[i-1]>='A' and s[i-1]<='Z':
         u+=1
-    else:
+
+    elif s[i-1]>='a' and s[i-1]<='z':
         l+=1
 
-
-if u==l:
+# print(u,l,x)
+if u==(x):
     print(st.upper())
+elif l==(x):
+    print(st.lower())
+elif u == l:
+    print(st.lower())
 else:
     print(st.lower())
