93b58a62c4a98f59edc5c9f02703ec27cc3f5edf
Pronob <pronobmozumder.info@gmail.com>
2020-10-15 15:48:05 +0600

    solved

---
+++ cf_155_a.py
@@ -0,0 +1,12 @@
+n = int(input())
+s = list(map(int,input().split()))[:n]
+l=[]
+m = []
+c = 0
+l.append(s[0])
+for i in range(1,n):
+    l.append(s[i])
+    if (min(l)== s[i] or max(l)==s[i]) and s[i] not in m:
+        c+=1
+        m.append(s[i])
+print(c)
