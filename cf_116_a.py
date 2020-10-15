8d686a68fcfb79ec34a8630abd47a012e593d9a6
Pronob <pronobmozumder.info@gmail.com>
2020-10-16 02:26:54 +0600

    solved

--- cf_116_a.py
+++ cf_116_a.py
@@ -1,11 +1,9 @@
 t = int(input())
-exit, enter = list(map(int,input().split()))
-minc= (enter - exit)
-max = minc
-for i in range(t-1):
+minc= 0
+max = 0
+for i in range(t):
     exit, enter = list(map(int,input().split()))
-    m = (minc-exit)+enter
-    if max<m:
-        max = m
+    minc += (enter - exit)
+    if max<minc:
+        max = minc
 print(max)
-
---
+++ cf_1391_a.py
@@ -0,0 +1,6 @@
+for i in range(int(input())):
+    n = int(input())
+    l = [int(k) for k in range(n+1)]
+    for j in range(n,0,-1):
+        print(j, end=" ")
+    print()
