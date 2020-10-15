125127bcf38cc55d1464c59a9a8edd78d26f74a5
Pronob <pronobmozumder.info@gmail.com>
2020-10-15 13:41:06 +0600

    solved with optimal

---
+++ cf_148_a.py
@@ -0,0 +1,12 @@
+k = int(input())
+l = int(input())
+m = int(input())
+n = int(input())
+d = int(input())
+
+r = 0
+
+for j in range(d):
+    if j%l==0 or j%m==0 or j%n==0 or j%k==0:
+        r+=1
+print(r)
