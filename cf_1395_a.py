16391cd84991198bcb8e617daae36032d14b1080
Pronob <pronobmozumder.info@gmail.com>
2020-10-16 18:12:17 +0600

    unsolved

--- cf_1395_a.py
+++ cf_1395_a.py
@@ -1,20 +1,72 @@
-f = 0
-o=0
+
+n = {}
 for i in range(int(input())):
     c = list(map(int,input().split()))
-    # print(c)
-    mi = min(c)
-    if mi!=0:
-        for i in range(1,len(c)-1):
-            if c[i-1]>=mi:
-                c[i-1]-=mi
-                if c[i-1]%2==1:
-                    o+=1
+    evn = 0
+    odd = 0
+    m = min(c)
+    for j in range(1,len(c)+1):
+        c[j-1]-=m
 
-    if o==1:
+
+    for k in range(1, len(c)+1):
+        if c[k-1]%2==0:
+            evn += 1
+        elif c[k-1]%2==1:
+            odd += 1
+    if (odd == 1 or odd==0):
         print("Yes")
     else:
         print("No")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # # print(c)
+    # mi = min(c)
+    # if mi!=0:
+    #     for i in range(1,len(c)-1):
+    #         if c[i-1]>=mi:
+    #             c[i-1]-=mi
+    #             if c[i-1]%2==1:
+    #                 o+=1
+    #
+    # if o==1:
+    #     print("Yes")
+    # else:
+    #     print("No")
 
 # o=0
 # e=0
