b60deccc12fba6ee8c6aaf051a156de1a8c57c21
Pronob <pronobmozumder.info@gmail.com>
2020-10-18 21:05:01 +0600

    solved

--- cf_219_a.py
+++ cf_219_a.py
@@ -1,24 +1,44 @@
-n = int(input())
-s = input()
-p = {}
-for i in range(1,len(s)+1):
-    if s[i-1] in p:
-        p[s[i-1]]+=1
-    else:
-        p[s[i-1]] = 1
-f=0
-for i in p.values():
-    if n==i:
-        f=1
-    else:
-        f=0
-        break
-
-if f == 1:
-    k = len(p.keys())
-    klist = [str(i) for i in p.keys()]
-    for i in range(len(s)//2):
-        print(*klist, sep="",end="")
-
+n=int(input())
+s=sorted(input())
+# print(s)
+x=s[::n]*n
+# print(x)
+if sorted(x)==s:
+    print("".join(x))
 else:
-    print("-1")
+    print(-1)
+
+
+
+
+
+# n = int(input())
+# s = input()
+# p = {}
+# for i in range(1,len(s)+1):
+#     if s[i-1] in p:
+#         p[s[i-1]]+=1
+#     else:
+#         p[s[i-1]] = 1
+#
+# # print(p)
+# f=0
+# for i in p.values():
+#     if n==i:
+#         f=1
+#     else:
+#         f=0
+#         break
+#
+# if f == 1:
+#     k = len(p.keys())
+#     # print(k)
+#     klist = [str(i) for i in p.keys()]
+#     # print(klist)
+#
+#     for i in range(n):
+#         print(*klist, sep="",end="")
+#
+#
+# else:
+#     print("-1")
--- cf_230_B.py
+++ cf_230_B.py
@@ -1,12 +1,29 @@
+import math
 input()
 s = list(map(int,input().split()))
 for j in range(1,len(s)+1):
     c = 0
-    n = s[j-1]//2
-    for k in range(2,n+1):
-        if s[j-1]%k==0:
-            c+=1
-    if c==1:
-        print("YES")
+    n = math.ceil(math.sqrt(s[j-1]))
+
+    cn= math.ceil(math.sqrt(n))
+    cf = 0
+    for i in range(1,cn+1):
+        if cn//i==0:
+            cf=0
+            break
+        else:
+            cf=1
+
+    if cf == 1:
+        for k in range(2,n+1):
+            if ((s[j-1]%k==0) and (n*n==s[j-1])):
+                if c<2:
+                    c+=1
+                else:
+                    break
+        if c==1:
+            print("YES")
+        else:
+            print("NO")
     else:
         print("NO")
