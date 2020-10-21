634f37f6662134209718a67ad0aeb9cb4ac4380c
Pronob <pronobmozumder.info@gmail.com>
2020-10-21 17:40:10 +0600

    solved

---
+++ a2oj Ladder - B/cf_447_B.py
@@ -0,0 +1,38 @@
+s=input()
+n=int(input())
+k=input().split()
+# print(k)
+max1=0
+for i in k:
+    if (int(i)> max1):
+        max1=int(i)
+sum=0
+# print(max1)
+l=1
+for i in s:
+    sum=sum+l*int(k[ord(i)-97])
+    l+=1
+
+# print(sum,l)
+for i in range(0,n):
+    sum=sum+l*max1
+    l+=1
+# print(sum,l)
+print(sum)
+# s1 = input()
+# p=s1
+# k = int(input())
+# s = list(map(int,input().split()))
+# ad = []
+# for i in range(1,len(s1)+1):
+#     ad.append(s[ord(s1[i-1])-97])
+#
+# print(ad)
+# print(k)
+# while(k!=0):
+#     if ad[k-1]>1 and ad[k-1]>=k:
+#         p=p+(p[k-1]*ad[k-1])
+#         k-=ad[k-1]
+#     print(k)
+#
+# print(p)
