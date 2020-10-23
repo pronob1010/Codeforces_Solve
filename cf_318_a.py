8671c05dea1cd97f6f8d6083a79e2e62bb984a1d
Pronob <pronobmozumder.info@gmail.com>
2020-10-24 05:13:37 +0600

    solved

--- cf_318_a.py
+++ cf_318_a.py
@@ -1,13 +1,20 @@
 a , b = list(map(int,input().split()))
+t = (a+1)//2
+if b<=t:
+    print(2*b-1)
+else:
+    print((b-t)*2)
 
-odd = []
-eve = []
-r = []
-for i in range(1,a+1):
-    if i%2==0:
-        eve.append(i)
-    else:
-        odd.append(i)
-
-r = odd+eve
-print(r[b-1])
+#
+# odd = []
+# eve = []
+# r = []
+# for i in range(1,a+1):
+#     if i%2==0:
+#         eve.append(i)
+#     else:
+#         odd.append(i)
+#
+# r = odd+eve
+# print(r)
+# # print(r[b-1])
