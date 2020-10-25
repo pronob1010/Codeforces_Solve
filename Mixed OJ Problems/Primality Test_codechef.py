29640f12deae339e888e713f3eeec39acf26dbdd
Pronob <pronobmozumder.info@gmail.com>
2020-10-25 23:19:27 +0600

    solved

--- Mixed OJ Problems/Primality Test_codechef.py
+++ Mixed OJ Problems/Primality Test_codechef.py
@@ -1,9 +1,10 @@
 #https://www.codechef.com/problems/PRB01
-for _ in range(int(input())):
+t = int(input())
+for j in range(t):
     n = int(input())
+    f = 0
     if n>1:
         i = 2
-        f = 0
         while(i*i<=n):
             if n%i==0:
                 print("no")
