a90869b802f22219b336b4a47bc960bda0276a84
Pronob <pronobmozumder.info@gmail.com>
2020-10-29 23:32:00 +0600

    solved

--- a2oj Ladder - B/cf_507_B.py
+++ a2oj Ladder - B/cf_507_B.py
@@ -1,26 +1,5 @@
+import math
 r,x,y,x1,y1 = list(map(int, input().split()))
 
-if x==x1 and y==y1:
-    print(0)
-
-elif x==x1:
-    p = y1 // (r*2)
-    if p<=r*2:
-        print(1)
-    else:
-        print(p)
-elif y==y1:
-    p = x1 // (r*2)
-    if p<=r*2:
-        print(1)
-    else:
-        print(p)
-
-else:
-    a1 = abs(x-x1)
-    a2 = abs(y-y1)
-
-    if a1==a2:
-        print(a1)
-    else:
-        print(min(a1,a2))
+ans = math.ceil(math.sqrt((x-x1)*(x-x1)+(y-y1)*(y-y1))/(2*r))
+print(ans)
