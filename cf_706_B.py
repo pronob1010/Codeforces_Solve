6ae00f19567e1bcdfe94ea67e0686b9815b40264
Pronob <pronobmozumder.info@gmail.com>
2020-11-05 02:48:19 +0600

    solved

--- cf_706_B.py
+++ cf_706_B.py
@@ -1,14 +1,35 @@
-n = int(input())
-x = list(map(int, input().split()))
-x.sort()
-q = int(input())
-coin = []
-for i in range(q):
-    coin.append(int(input()))
+def binarySearch(list, n):
+    first = 0
+    last = len(list)-1
+    r = -1
+    while(first<=last):
 
-for i in coin:
-    c = 0
-    for j in x:
-        if i>= j:
-            c+=1
-    print(c)
+        mid = (first + ((last - first) // 2))
+
+        if list[mid] <= n:
+            r = mid
+            first = mid+1
+
+        elif list[mid]>n:
+            last = mid-1
+
+    return r+1
+
+
+if __name__=='__main__':
+    n = int(input())
+    x = list(map(int, input().split()))
+    x.sort()
+    q = int(input())
+    coin = []
+    for i in range(q):
+        coin.append(int(input()))
+
+    # print(x)
+    # print(coin)
+    for i in coin:
+        c = binarySearch(x,i)
+        # for j in x:
+        #     if i>= j:
+        #         c+=11
+        print(c)
