6d3604051767b824ad85346a2c8377ce2de649ae
Pronob <pronobmozumder.info@gmail.com>
2020-11-07 20:13:45 +0600

    solved

---
+++ cf_1375_B.py
@@ -0,0 +1,6 @@
+# n = int(input())
+# for i in range(n):
+#     r,c = list(map(int, input().split()))
+#     for i in range(r):
+#         s = input()
+print("Google")
--- cf_1436_C.py
+++ cf_1436_C.py
@@ -1,40 +1,25 @@
+import math
+n, x, pos = map(int, input().split())
+left= 0
+right = n
 
-def binarrySeach(a,x):
-    left = 0
-    right = len(a)
-    while left<right:
-        middle = (left+right)//2
-        if a[middle]<= x:
-            left = middle+1
-        else:
-            right = middle
+more = n - x
+less = n - more - 1
+ans = 1
+while left < right:
+	mid = (left + right) // 2
+	if mid < pos:
+		ans *= less
+		less -= 1
+		left = mid + 1
 
-    if left>0 and a[left-1]==x:
-        return True
-    else:
-        return False
+	elif mid > pos:
+		ans *= more
+		more -= 1
+		right = mid
 
+	else:
+		left = mid + 1
 
-
-n,x,pos= list(map(int,input().split()))
-a = [int(i) for i in range(1,n+1)]
-for i in range(1,n+1):
-    print(binarrySeach(a,x))
-
-
-
-
-#
-# from itertools import permutations
-#
-# n,x,pos= list(map(int,input().split()))
-# comb = permutations(range(1,n+1), n)
-#
-# c = 0
-# # Print the obtained combinations
-# p = 0
-# for i in list(comb):
-#     l = list(i)
-#     if l[pos-1]==x:
-#         c+=1
-# print(c)
+ans *= math.factorial(more + less)
+print(ans % 1000000007)
