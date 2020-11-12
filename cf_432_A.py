7a59749875d569342a36bf3569cd4bd40ce89fea
Pronob <pronobmozumder.info@gmail.com>
2020-11-12 23:50:12 +0600

    solved

--- cf_432_A.py
+++ cf_432_A.py
@@ -4,19 +4,20 @@ s = list(map(int, input().split()))
 c = 0
 list = []
 for i in s:
-    if i<=5:
+    if i<=5-k:
         list.append(i)
-f={}
-for i in s:
-    if i in f:
-        f[i]+=1
-    else:
-        f[i]=1
-print(max(f.values()))
-max = max(f.values())
-if max<=k:
-    te=len(list)//3
-    if te<=k:
-        print(te)
-else:
-    print(0)
+# f={}
+# for i in s:
+#     if i in f:
+#         f[i]+=1
+#     else:
+#         f[i]=1
+# print(max(f.values()))
+# max = max(f.values())
+# if max<=k:
+#     te=len(list)//3
+#     if te<=k:
+#         print(te)
+# else:
+#     print(0)
+print(len(list)//3)
