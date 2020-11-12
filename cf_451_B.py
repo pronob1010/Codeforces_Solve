fe02751b5c972081fc62689e69d5918ca28414d7
Pronob <pronobmozumder.info@gmail.com>
2020-11-13 04:58:05 +0600

    Not setisfied

--- cf_451_B.py
+++ cf_451_B.py
@@ -1,18 +1,83 @@
-x = int(input())
-n = list(map(int, input().split()))
+n = int(input())
+arr = list(map(int, input().split()))
+xx  = sorted(arr)
 
-for i in range(1,len(n)+1):
-    if n[i-1]>n[i]:
-        n[i - 1],n[i] = n[i],n[i-1]
-        break
-
-s  = reversed(n)
-
-
-if s == n.sort():
-    print(s)
-    print(n)
-    print("yes")
-    print(s[0], s[1])
+if arr == xx:
+    print('yes')
+    print(1, 1)
 else:
-    print("no")
+    start , end = 0, 0
+    for i in range(0, n):
+        if xx[i] != arr[i]:
+            start = i
+            break
+
+    for j in range(n-1, -1, -1):
+        if xx[j] != arr[j]:
+            end = j
+            break
+
+    arr[start:end+1] = arr[start:end+1][::-1]
+    if xx == arr:
+        print('yes')
+        print(start+1, end+1)
+    else:
+        print('no')
+
+
+
+
+
+# x = int(input())
+# n = list(map(int, input().split()))
+#
+# sort_n = sorted(n)
+#
+# if n == sort_n:
+#     print("Yes")
+#     print(sort_n[0],sort_n[0])
+# else:
+#     start =0
+#     end = 0
+#
+#     for i in range(len(n)):
+#         if n[i-1]>n[i]:
+#             # print(i-1,i)
+#             n[i-1],n[i] = n[i],n[i-1]
+#             start = i-1
+#             end = i
+#             break
+#     if sort_n == n:
+#         print("yes")
+#         print(sort_n[start],sort_n[end])
+#     else:
+#         print("no")
+#
+#
+#
+# # nc = n
+# # nc.reverse()
+# # start =0
+# # end = 0
+# # # print(nc)
+# # for i in range(len(n)):
+# #     if n[i-1]>n[i]:
+# #         n[i-1],n[i] = n[i],n[i-1]
+# #         start = i-1
+# #         end = i
+# #         break
+# #
+# # s = n
+# # # print(s)
+# # x = sorted(n)
+# # # print(x)
+# # if nc == x:
+# #     # print(s)
+# #     # print(n)
+# #     print("yes")
+# #     print(s[start], s[end])
+# # elif s == x:
+# #     print("yes")
+# #     print(s[start], s[end])
+# # else:
+# #     print("no")
