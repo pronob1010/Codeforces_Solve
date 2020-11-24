2bc500bb1d0b9c4b9141141f311d08e3d44d38aa
Pronob <pronobmozumder.info@gmail.com>
2020-11-25 01:10:59 +0600

    unsolved

--- cf_1194_B.py
+++ cf_1194_B.py
@@ -1,37 +1,44 @@
+# t = int(input())
+# for l in range(t):
+#     x, y = list(map(int, input().split()))
+#
+#     mm = []
+#     f = 0
+#     g = 0
+#     for i in range(x):
+#         s = input()
+#         ms = []
+#         c = 0
+#         for j in range(len(s)):
+#             if s[j]=='*':
+#                 c+=1
+#         mm.append(c)
+#     r = 0
+#
+#     # print(mm)
+#     o = 0
+#     for i in range(len(mm)-1):
+#         if mm[i]==0:
+#             r+=1
+#         elif mm[i]>1 and mm[i]<y:
+#             r+=0
+#         # print(r)
+#
+#     r+=(y-mm[len(mm)-1])
+#     print(r)
+#
+#     #     if c>=1 and c<y:
+#     #         f +=1
+#     #     elif c == y:
+#     #         g +=1
+#     # print(f,g)
+#
+#
+#
+
 t = int(input())
 for l in range(t):
     x, y = list(map(int, input().split()))
 
-    mm = []
-    f = 0
-    g = 0
     for i in range(x):
         s = input()
-        ms = []
-        c = 0
-        for j in range(len(s)):
-            if s[j]=='*':
-                c+=1
-        mm.append(c)
-    r = 0
-
-    # print(mm)
-    o = 0
-    for i in range(len(mm)-1):
-        if mm[i]==0:
-            r+=1
-        elif mm[i]>1 and mm[i]<y:
-            r+=0
-        # print(r)
-
-    r+=(y-mm[len(mm)-1])
-    print(r)
-
-    #     if c>=1 and c<y:
-    #         f +=1
-    #     elif c == y:
-    #         g +=1
-    # print(f,g)
-
-
-
