f00d73a72c4db8fbfd477d08b0e75043bef3092a
Pronob <pronobmozumder.info@gmail.com>
2020-12-21 21:14:38 +0600

    Not optimal 0.1

--- cf_1411_B.py
+++ cf_1411_B.py
@@ -1,6 +1,4 @@
-t= int(input())
-for i in range(t):
-    s = int(input())
+def fun(s):
     while True:
         s1 = s
         while(s1>0):
@@ -10,6 +8,12 @@ for i in range(t):
             s1//=10
 
         if s1==0:
-            print(s)
+            return s
             break
         s+=1
+
+t= int(input())
+for i in range(t):
+    s = int(input())
+    print(fun(s))
+
