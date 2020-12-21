c03c92ef5c0e1898768c8a8fce92995245d96f09
Pronob <pronobmozumder.info@gmail.com>
2020-12-21 17:30:04 +0600

    Not optimal 0.1

--- cf_1411_B.py
+++ cf_1411_B.py
@@ -2,16 +2,14 @@ t= int(input())
 for i in range(t):
     s = int(input())
     while True:
-        s_copy = str(s)
-        dig = []
-        for i in range(len(s_copy)):
-            if s_copy[i]!='0' and s_copy[i] not in dig:
-                dig.append(int(s_copy[i]))
-        c = 0
-        for j in dig:
-            if s%j==0:
-                c+=1
-        if len(dig)==c:
+        s1 = s
+        while(s1>0):
+            r = s1%10
+            if(r != 0 and s%r != 0):
+                break
+            s1//=10
+
+        if s1==0:
             print(s)
             break
         s+=1
