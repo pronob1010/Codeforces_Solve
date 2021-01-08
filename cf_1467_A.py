8cf4e95b0eaf11457f2e1813dd2ea1e871faca5e
Pronob <pronobmozumder.info@gmail.com>
2021-01-08 23:55:35 +0600

    Done

---
+++ cf_1467_A.py
@@ -0,0 +1,15 @@
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    p1 = str(989)
+    for j in range(3):
+        if j<n:
+            print(p1[j],end="")
+    x = 0
+    for j in range(n-3):
+        if x>9:
+            x = 0
+        print(x, end="")
+        x+=1
+    print()
