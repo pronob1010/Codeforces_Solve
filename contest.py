eff581db2f926e875b7cc0fc7da54a6bdf4e8dfd
Pronob <pronobmozumder.info@gmail.com>
2021-01-08 23:52:53 +0600

    Done

--- contest.py
+++ contest.py
@@ -402,3 +402,13 @@ import math
 t = int(input())
 for i in range(t):
     n = int(input())
+    p1 = str(989)
+    for j in range(3):
+        if j<n:
+            print(p1[j],end="")
+    x = 0
+    for j in range(n-3):
+        if x>9:
+            x = 0
+        print(x, end="")
+    print()
